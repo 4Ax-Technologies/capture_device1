4a48113398a6ea9a41bc614aefba35ac03312676
unpickme <90104815+unpickme@users.noreply.github.com>
2024-02-27 17:00:59 +0000

    Update rc1_run.py

--- rc1_run.py
+++ rc1_run.py
@@ -2,7 +2,7 @@ pip install opencv-python
 
 pip install Jetson.GPIO
 
-###	sudo groupadd -f -r gpio  
+#	sudo groupadd -f -r gpio  
 ###	sudo usermod -a -G gpio  home directory_ 
  
 pip3 install pyautogui	
